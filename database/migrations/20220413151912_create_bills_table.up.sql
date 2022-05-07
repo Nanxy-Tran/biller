6f740c30b1917d96a5e8cbe781606556ae7a4bb6
nanxy.tran <eeit104@gmail.com>
2022-05-07 23:13:58 +0700

    change to GORM

--- database/migrations/20220413151912_create_bills_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS bills (
-                       id         INT AUTO_INCREMENT NOT NULL,
-                       name      VARCHAR(128) NOT NULL,
-                       amount     INTEGER NOT NULL,
-                       description   VARCHAR(255) DEFAULT 'description...',
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                       PRIMARY KEY (`id`)
-) ENGINE=INNODB;
--- database/migrations/20220413152351_create_tags_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tags (
-                            id         INT AUTO_INCREMENT NOT NULL,
-                            name      VARCHAR(128) NOT NULL,
-                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                            INDEX (id)
-) ENGINE=INNODB
